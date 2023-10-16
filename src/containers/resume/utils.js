1e2af1b04de659ec3a3f9516140ea85088c20c6c
SnehithaRechal <108850938+SnehithaRechal@users.noreply.github.com>
2023-10-16 22:28:30 +0530

    Add files via upload

---
+++ src/containers/resume/utils.js
@@ -0,0 +1,42 @@
+export const data = {
+    experience: [
+      {
+        title: "Consultant",
+        subTitle: "Miami, FL",
+        description:
+          " User Experience, Visual Design",
+      },
+      {
+        title: "Senior Consultant",
+        subTitle: "London, UK",
+        description:
+          "Web development, Databases, Backend developement",
+      },
+      {
+        title: "Project Manager",
+        subTitle: "Bangalore, INDIA",
+        description:
+          "Creative Direction Project Management, Team Leading",
+      },
+    ],
+    education: [
+      {
+        title: "Secondary",
+        subTitle: "Sri Chaitanya School",
+        description:
+          "Class -10",
+      },
+      {
+        title: "Intermediate",
+        subTitle: "Sasi Junior College",
+        description:
+          "Science stream",
+      },
+      {
+        title: "Bachelor of Technology",
+        subTitle: "VIT institute of technology, Amaravati.",
+        description:
+          "Bachelor of technology in computer science",
+      },
+    ],
+  };
